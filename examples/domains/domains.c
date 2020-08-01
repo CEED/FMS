53455a1c447cc9f908b3ba0f20f1e8533b0e4bea
Brad Whitlock <bjw@ilight.com>
2020-07-31 18:02:12 -0700

    Added option for order 2 coordinates.

--- examples/domains/domains.c
+++ examples/domains/domains.c
@@ -18,6 +18,7 @@
 #include <fms.h>
 #include <fmsio.h>
 #include <stdlib.h>
+#include <stdio.h>
 
 /* We'll make multiple domains and components/parts. */
 
@@ -131,14 +132,77 @@ int d3_tri_edge[] = {
     5,6,8  /* 3 */
 };
 
-const int *reorder[]={0,0,0,0,0,0,0,0,0};
+double *
+append_domain_coords(int dom, double *ptr, const int *global_index, int nverts)
+{
+    int i;
+    double pt[2];
+    for(i = 0; i < nverts; i++)
+    {
+        int gidx = global_index[i];
+        pt[0] = global_coords[2*gidx];
+        pt[1] = global_coords[2*gidx+1];
+
+        *ptr++ = pt[0];
+        *ptr++ = pt[1];
+
+        /* Print value to Point3D output. */
+        printf("%lg %lg 0. %d\n", pt[0],pt[1], dom);
+    }
+    return ptr;
+}
+
+double *
+append_domain_coords_edge(int dom, double *ptr, const int *global_index, int verts,
+    const int *edges, int nedges)
+{
+    int i, v0, v1;
+    double xoffset, yoffset, pt[2], travel = 0.02;
+
+    for(i = 0; i < nedges; ++i)
+    {
+        /* Get the i'th edge and get the global indices of its vertices. */
+        v0 = global_index[edges[2*i]];
+        v1 = global_index[edges[2*i+1]];
+
+        /* Figure out how much to perturb the point. */
+        switch((v0+v1) % 8)
+        {
+        case 0: xoffset = -travel; yoffset = 0;       break;
+        case 1: xoffset = -travel; yoffset = -travel; break;
+        case 2: xoffset = 0.;      yoffset = -travel; break;
+        case 3: xoffset = travel;  yoffset = -travel; break;
+        case 4: xoffset = travel;  yoffset = 0;       break;
+        case 5: xoffset = travel;  yoffset = travel;  break;
+        case 6: xoffset = 0;       yoffset = travel;  break;
+        case 7: xoffset = -travel; yoffset = travel;  break;
+        }
+
+        /* Compute the edge midpoint and perturb it. */
+        pt[0] = 0.5 * (global_coords[v0*2+0] + global_coords[v1*2+0]) + xoffset;
+        pt[1] = 0.5 * (global_coords[v0*2+1] + global_coords[v1*2+1]) + yoffset;
+
+        *ptr++ = pt[0];
+        *ptr++ = pt[1];
+
+        /* Print value to Point3D output. */
+        printf("%lg %lg 0. %d\n", pt[0],pt[1], dom);
+    }
+
+    return ptr;
+}
 
 int
 main(int argc, char *argv[])
 {
     const char *protocol = "ascii";
+    FmsInt order = 1;
     if(argc > 1)
+    {
         protocol = argv[1];
+        if(argc > 2)
+            order = atoi(argv[2]);
+    }
 
     /* Create a mesh */
     FmsMesh mesh;
@@ -208,7 +272,6 @@ main(int argc, char *argv[])
     FmsFieldDescriptor coords_fd;
     FmsDataCollectionAddFieldDescriptor(dc, "coords descriptor", &coords_fd);
     FmsFieldDescriptorSetComponent(coords_fd, surface); /* define coords over surface */
-    FmsInt order = 1;
     FmsFieldDescriptorSetFixedOrder(coords_fd, FMS_CONTINUOUS,
                                     FMS_NODAL_GAUSS_CLOSED, order);
 
@@ -216,34 +279,42 @@ main(int argc, char *argv[])
        is defined over the "surface" component, we make a new field 
        that contains values from all domains, with their local vertex ordering.
      */
-    int ntotal_verts = nverts[0] + nverts[1] + nverts[2] + nverts[3];
-    double *coord_data = (double *)malloc(ntotal_verts * sizeof(double)*2);
+    int ndofs[4] = {0,0,0,0};
+    if(order == 1)
+    {
+        ndofs[0] = nverts[0];
+        ndofs[1] = nverts[1];
+        ndofs[2] = nverts[2];
+        ndofs[3] = nverts[3];
+    }
+    else if(order == 2)
+    {
+        ndofs[0] = nverts[0] + nedges[0];
+        ndofs[1] = nverts[1] + nedges[1];
+        ndofs[2] = nverts[2] + nedges[2];
+        ndofs[3] = nverts[3] + nedges[3];
+    }        
+    int ntotal_dofs = ndofs[0] + ndofs[1] + ndofs[2] + ndofs[3];
+    double *coord_data = (double *)malloc(ntotal_dofs * sizeof(double)*2);
     double *cptr = coord_data;
-    int i;
-    for(i = 0; i < nverts[0]; i++)
-    {
-        int gidx = d0_global_index[i];
-        *cptr++ = global_coords[2*gidx];
-        *cptr++ = global_coords[2*gidx+1];
-    }
-    for(i = 0; i < nverts[1]; i++)
-    {
-        int gidx = d1_global_index[i];
-        *cptr++ = global_coords[2*gidx];
-        *cptr++ = global_coords[2*gidx+1];
-    }
-    for(i = 0; i < nverts[2]; i++)
-    {
-        int gidx = d2_global_index[i];
-        *cptr++ = global_coords[2*gidx];
-        *cptr++ = global_coords[2*gidx+1];
-    }
-    for(i = 0; i < nverts[3]; i++)
-    {
-        int gidx = d3_global_index[i];
-        *cptr++ = global_coords[2*gidx];
-        *cptr++ = global_coords[2*gidx+1];
-    }
+
+    /* Write a Point3D header since we will write coord values to stdout in a plottable way. */
+    printf("X Y Z domain\n");
+
+    /* Write the coordinate data. */
+    cptr = append_domain_coords(0, cptr, d0_global_index, nverts[0]);
+    if(order == 2)
+        cptr = append_domain_coords_edge(0, cptr, d0_global_index, nverts[0], d0_edge_vert, nedges[0]);
+    cptr = append_domain_coords(1, cptr, d1_global_index, nverts[1]);
+    if(order == 2)
+        cptr = append_domain_coords_edge(1, cptr, d1_global_index, nverts[1], d1_edge_vert, nedges[1]);
+    cptr = append_domain_coords(2, cptr, d2_global_index, nverts[2]);
+    if(order == 2)
+        cptr = append_domain_coords_edge(2, cptr, d2_global_index, nverts[2], d2_edge_vert, nedges[2]);
+    cptr = append_domain_coords(3, cptr, d3_global_index, nverts[3]);
+    if(order == 2)
+        cptr = append_domain_coords_edge(3, cptr, d3_global_index, nverts[3], d3_edge_vert, nedges[3]);
+
     /*Q: suppose that the coordinate order was 2+, would the extra dofs go here 
          or would they appear after their individual domain loops above?
      */
@@ -253,7 +324,6 @@ main(int argc, char *argv[])
 
     /* Set the coordinates for the component. */
     FmsComponentSetCoordinates(surface, coords);
-
 
     /* Write the data out. */
     FmsIOWrite("domains.fms", protocol, dc);
